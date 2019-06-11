5a4b95ebb183b77ce88644ad4f20407c3636850d
Scott <stsmith@callutheran.edu>
2019-06-11 10:58:08 -0700

    moved actions to mobx store

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+import { observer } from "mobx-react";
+import modelStore from "./src/state.js";
+
 import './src/assets/sass/pitwall.scss';
 
 import PageHeader from "./src/components/PageHeader";
@@ -8,13 +10,14 @@ import ConstructorStandings  from "./src/components/ConstructorStandings";
 import DriverStandings from "./src/components/DriverStandings";
 import RaceResults from "./src/components/RaceResults";
 
+
+@observer
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.changeSeason = this.changeSeason.bind(this);
-        this.changeTab = this.changeTab.bind(this);
         this.currentYear = new Date().getFullYear();
+        this.modelStore = this.props.modelStore;
         this.state = {
             races: [],
             driverStandings: [],
@@ -23,114 +26,38 @@ class App extends React.Component {
             selectedTab: 'races',
             loading: true,
             error: false
-          
         }
-    }
 
-    changeSeason(newSeason){
-        this.getData(newSeason);
-    }
-
-    changeTab(newTab){
-        this.setState({
-            selectedTab: newTab
-        });
-    }
-
-    getData(loadYear){
-	    
-	    this.setState({
-			seasonSelect: loadYear
-	    });
-        
-        let $this = this;
-
-        const apiEndpoints = [
-            'results',
-            'driverStandings',
-            'constructorStandings'
-        ];
-
-        apiEndpoints.forEach((apiData) => {
-
-            axios.get('https://ergast.com/api/f1/' + loadYear + '/' + apiData + '.json?limit=1000')
-                .then(function(response){
-                    let ajax_data = response.data.MRData;
-                    
-                    if(apiData == 'results'){
-                        $this.setState({
-                            races: ajax_data.RaceTable.Races
-                        });
-                    }
-
-                    if(apiData == 'driverStandings'){
-                        let driversData = (ajax_data.StandingsTable.StandingsLists.length > 0) ? ajax_data.StandingsTable.StandingsLists[0].DriverStandings : [];
-
-                        $this.setState({
-                            driverStandings: driversData
-                        });
-                    }
-                    
-                    if(apiData == 'constructorStandings'){
-                        let constructorsData = (ajax_data.StandingsTable.StandingsLists.length > 0) ? ajax_data.StandingsTable.StandingsLists[0].ConstructorStandings : [];
-                        
-                        $this.setState({
-                             constructorStandings: constructorsData
-                        });
-                    }
-					
-					$this.setState({
-				   		loading: false
-	            		});
-                })
-                .then(function(){
-	               
-                })
-                .catch(function(){
-					// error
-                });
-        });
-        
-        
-    }
-
-    componentDidMount(){
-        this.getData(this.currentYear);
+        this.modelStore.getData(this.currentYear);
     }
 
     render() {
 
         return (
             <div id="page">
-                <PageHeader onSeasonSelect={this.changeSeason} onChangeTab={this.changeTab}></PageHeader>
+                <PageHeader></PageHeader>
 
                 <main className="content-area">
 
                     <header className="content-header content-block">
-                        <h1>{this.state.seasonSelect} Season</h1>
+                        <h1>{this.modelStore.viewOptions.seasonSelect} Season</h1>
                     </header>
                     
                     <section className="content-block">
                         	
-                        	{this.state.selectedTab == 'races' &&
-							<RaceResults races={this.state.races} />
+                        {this.modelStore.viewOptions.selectedTab == 'races' &&
+                            <RaceResults />
 						}
 						
-						{this.state.selectedTab == 'drivers' &&
-							<DriverStandings standings={this.state.driverStandings} />
-        					} 
+                        {this.modelStore.viewOptions.selectedTab == 'drivers' &&
+							<DriverStandings />
+        				} 
 						
-						{this.state.selectedTab == 'constructors' &&
-							<ConstructorStandings standings={this.state.constructorStandings} />
+                        {this.modelStore.viewOptions.selectedTab == 'constructors' &&
+							<ConstructorStandings />
 						}
                         	
                     </section>
-
-                    {/* {this.state.loading == true &&
-			            <section className="overlay">
-			                <div className="loading-spinner"></div>
-			            </section>
-			        } */}
 
                 </main>
 
@@ -139,5 +66,5 @@ class App extends React.Component {
     }
 }
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+
+ReactDOM.render(<App modelStore={modelStore} />, document.getElementById("app"));
