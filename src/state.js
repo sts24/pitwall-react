623c2c3d79f902cdfcf035de7b0d73b56229facd
Scott <stsmith@callutheran.edu>
2019-06-13 13:24:32 -0700

    async axios api call

--- src/state.js
+++ src/state.js
@@ -25,28 +25,42 @@ class dataStore {
 
 	@action getData(loadYear) {
 
+		let $this = this;
+
 		this.setViewOptions({
 			'seasonSelect': loadYear
 		});
 
-		axios.get('https://ergast.com/api/f1/' + loadYear + '/results.json?limit=1000')
-			.then(response => {
-				let races = response.data.MRData.RaceTable.Races;
-				this.setRaces(races);
-			});
+		function getRaceData(loadYear) {
+			return axios.get('https://ergast.com/api/f1/' + loadYear + '/results.json?limit=1000')
+				.then(response => {
+					let races = response.data.MRData.RaceTable.Races;
+					return races;
+				});
+		}
 
-		axios.get('https://ergast.com/api/f1/' + loadYear + '/driverStandings.json?limit=1000')
-			.then(response => {
-				let driversData = (response.data.MRData.StandingsTable.StandingsLists.length > 0) ? response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings : [];
-				this.setDrivers(driversData);
-			});
+		function getDriversData(loadYear) {
+			return axios.get('https://ergast.com/api/f1/' + loadYear + '/driverStandings.json?limit=1000')
+				.then(response => {
+					let driversData = (response.data.MRData.StandingsTable.StandingsLists.length > 0) ? response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings : [];
+					return driversData;
+				});
+		}
 
-		axios.get('https://ergast.com/api/f1/' + loadYear + '/constructorStandings.json?limit=1000')
-			.then(response => {
-				let constructorsData = (response.data.MRData.StandingsTable.StandingsLists.length > 0) ? response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings : [];
-				this.setConstructors(constructorsData);
-			});
+		function getConstructorsData(loadYear) {
+			return axios.get('https://ergast.com/api/f1/' + loadYear + '/constructorStandings.json?limit=1000')
+				.then(response => {
+					let constructorsData = (response.data.MRData.StandingsTable.StandingsLists.length > 0) ? response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings : [];
+					return constructorsData;
+				});
+		}
 
+		axios.all([getRaceData(loadYear), getDriversData(loadYear), getConstructorsData(loadYear)])
+			.then(axios.spread(function (raceData, driversData, constructorsData) {
+				$this.setRaces(raceData);
+				$this.setDrivers(driversData);
+				$this.setConstructors(constructorsData);
+			}));
 		
 	}
 }
